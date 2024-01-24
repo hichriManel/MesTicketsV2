ac68fad324ef44efaa373fafdc20d079a24bfc9d
Halim Choukani <halim.choukani@gmail.com>
2024-01-25 00:28:36 +0100

    Refactor account type selection in editprofile.php

--- controller/compte/editprofile.php
+++ controller/compte/editprofile.php
@@ -24,25 +24,32 @@ if (isset($_SESSION['email'])) {
 
 
 <div class="col-md-6">
-    <label class="form-label">Type de Compte</label>
-    <div class="row">
-        <div class="col-md-6">
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="typec" value="client">
-                <label class="form-check-label">
-                    client
-                </label>
+    <?php
+    if ($_SESSION["type"] == 'supervisor') {
+    ?>
+        <label class="form-label">Type de Compte</label>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="typec" value="client">
+                    <label class="form-check-label">
+                        client
+                    </label>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="typec" value="admin" checked="checked">
+                    <label class="form-check-label">
+                        Admin
+                    </label>
+                </div>
             </div>
         </div>
-        <div class="col-md-6">
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="typec" value="admin" checked="checked">
-                <label class="form-check-label">
-                    Admin
-                </label>
-            </div>
-        </div>
-    </div>
+    <?php
+    }
+
+    ?>
 </div>
 <div class="row g-3 align-items-center">
     <div class="col-md-6">
