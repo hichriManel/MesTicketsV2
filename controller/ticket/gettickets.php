7670f25c351f0cc3029b83e0533cbe5b5bddfd5a
Halim Choukani <halim.choukani@gmail.com>
2024-01-25 09:51:52 -0800

    ezgzegz

--- controller/ticket/gettickets.php
+++ controller/ticket/gettickets.php
@@ -98,7 +98,7 @@ if ($_SESSION["type"] == "client") {
         }
       }
       if($_SESSION["type"]=="supervisor"){
-        echo "<td><a href='supprimer.php?id=" . $row[0] . "'><button class='btn btn-danger'>Supprimer</button></a></td>";
+        echo "<td><a href='supprimer.php?id=" . $row[0] . "'><button class='btn btn-danger text-white'>Supprimer</button></a></td>";
       }
       echo "</tr>";
     }
